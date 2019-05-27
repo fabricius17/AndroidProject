c7893fa4cf054869544532da3a8cc17f2d499ecd
fabricius17 <32256725+fabricius17@users.noreply.github.com>
2019-05-27 15:11:40 +0200

    last commit on this branch

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidproject;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -14,11 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        disableStrictMode();
+
     }
 
+    public void disableStrictMode(){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
     public void getMovies(View view) {
+//        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskReads();
+//        try {
+            TmdbApi api = new TmdbApi("ea786a1de5ea2977157afe4085e0bb07");
+
+//        } finally {
+//            StrictMode.setThreadPolicy(oldPolicy);
+//        }
         TmdbMovies movies = new TmdbApi("ea786a1de5ea2977157afe4085e0bb07").getMovies();
         MovieDb movie = movies.getMovie(5353,"en");
-        System.out.println("TEST OF API:\t"+movie.getHomepage());
+        System.out.println("TEST OF API:\t"+movie.getOverview());
     }
 }
--- app/src/main/java/com/example/androidproject/tester.java
+++ app/src/main/java/com/example/androidproject/tester.java
@@ -11,9 +11,20 @@ public class tester {
     public static void main(String[] args) {
         TmdbMovies movies = new TmdbApi("ea786a1de5ea2977157afe4085e0bb07").getMovies();
         MovieResultsPage mrp = movies.getPopularMovies("en", 0);
-        System.out.println(mrp.getResults());
-        List<MovieDb> l = mrp.getResults();
-        System.out.println(((MovieDb)(l.get(0))).);
+//        System.out.println(mrp.getResults());
+        List<MovieDb> list = mrp.getResults();
+
+        int id = list.get(0).getId();
+//        int id = 5353;
+        System.out.println(id);
+        MovieDb movie = movies.getMovie(id, "en");
+        System.out.println("ReleaseDate:\t" + movie.getReleaseDate());
+        System.out.println("Homepage:\t" + movie.getHomepage());
+        System.out.println("Cast:\t" + movie.getCast());
+        System.out.println("Budget:\t" + movie.getBudget());
+        System.out.println("Genres:\t" + movie.getGenres());
+        System.out.println("Overview:\t" + movie.getOverview());
+
 
 //        System.out.println(mrp.getTotalPages());
 //        mrp = movies.getPopularMovies("en",1);
