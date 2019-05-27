794dc67e5fe7d7bf98d279d7f3c0fd75fcaed7f5
Rickie Andersen <rande17@student.sdu.dk>
2019-05-27 14:02:22 +0200

    for loop in testing

--- app/src/main/java/com/example/androidproject/tester.java
+++ app/src/main/java/com/example/androidproject/tester.java
@@ -1,27 +1,18 @@
 package com.example.androidproject;
 
-import java.util.List;
-
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
-import info.movito.themoviedbapi.model.MovieDb;
 import info.movito.themoviedbapi.model.core.MovieResultsPage;
 
 public class tester {
     public static void main(String[] args) {
         TmdbMovies movies = new TmdbApi("ea786a1de5ea2977157afe4085e0bb07").getMovies();
         MovieResultsPage mrp = movies.getPopularMovies("en", 0);
-        System.out.println(mrp.getResults());
-        List<MovieDb> l = mrp.getResults();
-        System.out.println(((MovieDb)(l.get(0))).);
 
-//        System.out.println(mrp.getTotalPages());
-//        mrp = movies.getPopularMovies("en",1);
-//        System.out.println(mrp.getResults());
-//        mrp = movies.getPopularMovies("en",2);
-//        System.out.println(mrp.getResults());
-//        mrp = movies.getPopularMovies("en",3);
-//        System.out.println(mrp.getResults());
+        for (int i = 0; i < mrp.getResults().size(); i++) {
+            int id = mrp.getResults().get(i).getId();
+            System.out.println("index: " + i + "\tID: " + id + " - " + mrp.getResults().get(i));
+        }
 
     }
 }
