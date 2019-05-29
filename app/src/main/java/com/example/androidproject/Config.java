aa4ae64d276ad7142bc988f9501bd69e4066de1e
Rickie Andersen <rande17@student.sdu.dk>
2019-05-29 15:17:44 +0200

    Async done'ish, don't know whats needed but can easily be expanded.

---
+++ app/src/main/java/com/example/androidproject/Config.java
@@ -0,0 +1,7 @@
+package com.example.androidproject;
+
+class Config {
+    static String getAPI() {
+        return "ea786a1de5ea2977157afe4085e0bb07";
+    }
+}
--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,24 +1,71 @@
 package com.example.androidproject;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
 import info.movito.themoviedbapi.model.MovieDb;
 
 public class MainActivity extends AppCompatActivity {
+    int movieid = 5350;
+    TmdbMovies movies;
+    MovieDb movie;
 
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new AsyncTmdbMovies().execute();
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
     }
 
     public void getMovies(View view) {
-        TmdbMovies movies = new TmdbApi("ea786a1de5ea2977157afe4085e0bb07").getMovies();
-        MovieDb movie = movies.getMovie(5353,"en");
-        System.out.println("TEST OF API:\t"+movie.getHomepage());
+        new AsyncMovieDb().execute(movieid);
+    }
+
+    public class AsyncTmdbMovies extends AsyncTask<TmdbMovies, Integer, TmdbMovies> {
+
+
+        @Override
+        protected TmdbMovies doInBackground(TmdbMovies[] moviesArray) {
+            if (movies == null) {
+                movies = new TmdbApi(Config.getAPI()).getMovies();
+            }
+            return movies;
+        }
+    }
+
+    public class AsyncMovieDb extends AsyncTask<Integer, Integer, MovieDb> {
+        MovieDb getMovie(int movid) {
+            System.out.println(movid);
+            try {
+                movie = movies.getMovie(movid, "en");
+            } catch (RuntimeException e) {
+                movid++;
+                return (getMovie(movid));
+            }
+            movieid = movid;
+            return movie;
+        }
+
+        @Override
+        protected MovieDb doInBackground(Integer... movid) {
+            getMovie(movid[0]);
+            System.out.println("ID: " + movieid + " " + movie.toString());
+            return movie;
+        }
+
+
+        @Override
+        protected void onPostExecute(MovieDb movie) {
+            movieid++;
+            TextView tv = findViewById(R.id.tv);
+            tv.setText(movie.getTitle());
+        }
     }
 }
